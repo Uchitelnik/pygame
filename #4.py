78fffd75b1125f35ae7a8e007bb74300b9799cfd
Uchitelnik <speedcuber-g0@mail.ru>
2026-04-19 21:16:11 +0300

    Создали , пока выображаемую переменную координат. И сделали чтобы персонаж двигался , а также сделали чтобы можно было включать и выключать звук. и сделали условие соприкосновение с границей

--- #4.py
+++ #4.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import os
 
+from pygame.examples.sprite_texture import renderer
+
 pygame.init()
 pygame.mixer.init()
 weight = 1024
@@ -21,6 +23,16 @@ PURPLE = (106, 90, 205)
 WHITEGREEN = (0 , 255 , 0)
 GREEN = (60, 179, 113)
 
+def draw(weight , height , color , text = ''):
+    okno = pygame.Surface((weight , height))
+    okno.fill(WHITE)
+    if text:
+        txt = pygame.font.Font(None , 36)
+        renedrer_text = txt.render(text , True , BLUEGREEN)
+        cord_text = renedrer_text.get_rect(center = (weight // 2 , height // 2))
+        okno.blit(renedrer_text , cord_text)
+    return okno
+
 def protect_file(filename , size = None):
     try:
         script = os.path.dirname(os.path.abspath(__file__))
@@ -31,9 +43,14 @@ def protect_file(filename , size = None):
             image = pygame.transform.scale(image , size)
         print("1")
         return image
+    except FileNotFoundError as e:
+        print(f"Неудалось загрузить изображение {filename}: {e}")
+        return None
     except pygame.error as e:
         print(f"Неудалось загрузить изображение {filename}: {e}")
         return None
+
+
 
 def music_file(filename):
     try:
@@ -56,7 +73,15 @@ def loading(filename):
         print(e)
         return None
 
-person = protect_file("ninja_warrior.png" , (512 , 512))
+phon = protect_file("images.jpg" , (1024 , 768))
+if phon is None:
+    phon = draw(150 , 150 , PURPLE , "phon")
+person = protect_file("pngwing.com (1).png" , (100, 100))
+if person is None:
+    pesron = draw(150 , 150 , PURPLE , "person")
+enemy = protect_file("pngwing.com.png" , (250 , 250))
+if enemy is None:
+    enemy = draw(150 , 150 , PURPLE , "person")
 music = music_file("Plants_vs._Zombies_OST_-_Roof_Theme_(SkySound.cc).mp3")
 load_music = loading("28 Days Later Main Theme (From 28 Days Later).mp3")
 
@@ -68,14 +93,44 @@ pygame.mixer.music.set_volume(0.4)
 console = True
 clock = pygame.time.Clock().tick(120)
 
+cord_person = [128 , 300]
+scale_person = 1.0
+
 while console:
     for event in pygame.event.get():
         if pygame.QUIT == event.type:
             console = False
-    okno.fill(GREEN)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                cord_person[1] -= 10
+            elif event.key == pygame.K_s:
+                cord_person[1] += 10
+            elif event.key == pygame.K_a:
+                cord_person[0] -= 10
+            elif event.key == pygame.K_d:
+                cord_person[0] += 10
+            elif event.key == pygame.K_e:
+                if scale_person == 1.0:
+                    scale_person = 0.5
+                else:
+                    scale_person = 1.0
+            elif event.key == pygame.K_m:
+                if pygame.mixer.music.get_busy():
+                    pygame.mixer.music.pause()
+                else:
+                    pygame.mixer.music.unpause()
+    if cord_person[0] >= weight - 100:
+    if cord_person[1] >= height - 100:
+
+    if phon:
+        okno.blit(phon , (0 , 0))
     if person:
-        okno.blit(person , (128 , 128))
+        okno.blit(person , (128 , 300))
+    if enemy:
+        okno.blit(enemy , (600 , 165))
     pygame.display.flip()
+
+
 
 
 pygame.quit()
