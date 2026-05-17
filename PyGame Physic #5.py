74791c9bc4ccda6db3b38db812d33989bcb79f4a
Uchitelnik <speedcuber-g0@mail.ru>
2026-05-17 21:11:38 +0300

    Сделали классы: Particle - который вносит в себя функции (is_alive , draw , update) ,Physic_Sim - который вносит в себя функции(create_balls , add_balls)

--- PyGame Physic #5.py
+++ PyGame Physic #5.py
@@ -58,12 +58,12 @@ class Ball:
         self.y += self.vy * dt
         self.ax = 0
         self.ay = 0
-        self.trail.append((int(self.x) , int(self.y)))
+        self.trail.append((int(self.x), int(self.y)))
         if len(self.trail) >= self.trail_len:
             self.trail.pop(0)
         self.check_adg()
 
-    def check_adg(self): #функция для проверки столкновение с границей
+    def check_adg(self):  # функция для проверки столкновение с границей
         if self.y + self.radius > height:
             self.y = height - self.radius
             self.vy = -self.vy * DAMPING
@@ -78,14 +78,68 @@ class Ball:
             self.x = self.radius
             self.vx = -self.vx * DAMPING
 
-    def trail_draw(self , surface):
-        for i ,pos in enumerate(self.trail):
+    def trail_draw(self, surface):
+        for i, pos in enumerate(self.trail):
             alpha = int(255 * (i / len(self.trail))) if self.trail else 255
             color = (*self.color, alpha)
-            pygame.draw.circle(surface , self.color , pos , max(1 , self.radius // 4))
-        pygame.draw.circle(surface , self.color , (int(self.x) , int(self.y)) , self.radius)
-        pygame.draw.circle(surface , BLACK , (int(self.x) , int(self.y)) , self.radius , 6)
+            pygame.draw.circle(surface, self.color, pos, max(1, self.radius // 4))
+        pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.radius)
+        pygame.draw.circle(surface, BLACK, (int(self.x), int(self.y)), self.radius, 6)
         if abs(self.vx) > 1 or abs(self.vy) > 1:
             end_x = self.x + self.vx * 0.1
             end_y = self.y + self.vy * 0.1
-            pygame.draw.line(surface , GREEN , (self.x , self.y) , (end_x , end_y) ,5)
+            pygame.draw.line(surface, GREEN, (self.x, self.y), (end_x, end_y), 5)
+
+
+class Particle:
+    def __init__(self, x, y, vx, vy, color, lifetime=3):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        self.lifetime = lifetime
+        self.size = random.randint(5, 15)
+        self.age = 0
+
+    def is_alive(self):
+        return self.age < self.lifetime
+
+    def draw(self, surface):
+        alpha = max(0, 1 - (self.age / self.lifetime))
+        pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.size)
+
+    def update(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+        self.vy += GRAVITY * 0.5 * dt
+        self.age += dt
+
+
+class Physic_Sim:
+    def __init__(self):
+        self.balls = []
+        self.particales = []
+        self.gravity_enabled = True
+        self.show_vector = True
+        self.paused = False
+        self.create_balls()
+
+    def create_balls(self):
+        ball1 = Ball(200, 150, 40, ORANGE)
+        ball1.vy = 120
+        self.balls.append(ball1)
+        ball2 = Ball(100, 400, 30, DARKBLUE)
+        ball2.vx = 85
+        ball2.vy = -50
+        self.balls.append(ball2)
+        ball3 = Ball(30, 300, 50, RED)
+        self.balls.append(ball3)
+
+    def add_balls(self , x , y):
+        radius = random.randint(20 , 80)
+        color = random.choice([WHITE , ORANGE , TURQUOISE , RED , BLACK , DARKBLUE , BLUE , PURPLE , WHITEGREEN , GREEN])
+        ball = Ball(x , y ,radius , color)
+        ball.vx = random.randint(-300 , 300)
+        ball.vy = random.randint(-200 , 300)
+        self.balls.append(ball)
