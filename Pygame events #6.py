396d333bd2464b443b83aac3eee482d136145e0a
Uchitelnik <speedcuber-g0@mail.ru>
2026-06-06 21:11:47 +0300

    𝚂𝚘𝚣𝚍𝚊𝚕𝚒 𝚗𝚘𝚟𝚢𝚢 𝚙𝚛𝚘𝚎𝚔𝚝 , 𝚒 𝚗𝚊𝚙𝚒𝚜𝚊𝚕𝚒 𝚝𝚊𝚖 𝚔𝚕𝚊𝚜𝚜 𝚋𝚊𝚕𝚕 𝚒 𝚗𝚎𝚖𝚗𝚘𝚐𝚘 𝚔𝚕𝚊𝚜𝚜𝚊 𝙺𝚎𝚢𝙱𝚞𝚝𝚝𝚘𝚗

---
+++ Pygame events #6.py
@@ -0,0 +1,60 @@
+from tty import ISPEED
+
+import pygame
+import sys
+import random
+
+pygame.init()
+width = 1024
+height = 1024
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Screen")
+
+BLUEGREEN = (30, 147, 123)
+WHITE = (255, 255, 255)
+ORANGE = (255, 165, 0)
+TURQUOISE = (0, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+DARKBLUE = (65, 79, 113)
+BLUE = (0, 0, 255)
+PURPLE = (106, 90, 205)
+WHITEGREEN = (0, 255, 0)
+GREEN = (60, 179, 113)
+
+
+class Ball:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 64
+        self.color = ORANGE
+        self.dx = 0
+        self.dy = 0
+        self.speed = 2
+
+    def update(self):
+        self.x = self.x + self.dx
+        self.y = self.y + self.dy
+        if self.x + self.size or self.y + self.size == 1024:
+            self.x, self.y = 0, 0
+        elif self.x - self.size or self.y - self.size == 0:
+            self.x, self.y = 0, 0
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (self.x, self.y), self.size)
+
+    def movement(self, dx, dy):
+        self.dx = dx * self.speed
+        self.dy = dy * self.speed
+
+
+class Keybutton:
+    def __init__(self, x, y, width, height, color, text):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.color_on = RED
+        self.is_clicked = False
+        self.is_hovered = False
+        self.font = pygame.font.Font("Monospace" , 10)
